d30a7a1f0fe8e94f8be812796a97e7116a8ac2e8
Gaines Yeilding <gfy613@gmail.com>
2020-07-18 16:42:39 -0400

    Updated Schema, Added Seeds, JS in progress

--- employeeTracker.js
+++ employeeTracker.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
-  port: 8080,
+  port: 3306,
 
   // Your username
   user: "root",
@@ -36,7 +36,8 @@ function start() {
       // based on their answer, either call the bid or the post functions
       if (answer.selectionChoice === "View All Employees by Department") {
         viewDepartment();
-       
+        
+            
       }
       else if(answer.selectionChoice === "View all Employees by Manager") {
         viewManager();
@@ -57,201 +58,145 @@ function start() {
       }
       else if(answer.selectionChoice === "View All Roles") {
         viewAll();
+        start();
         
       }      
       else{
         connection.end();
       }
     });
-}
+};
 
 
-// function to view all employees
+// function to view all employees 
 
 function viewAll(){
-  connection.query("SELECT * FROM employees", function(err, results) {
-    var employeeArray = [];
-            for (var i = 0; i < results.length; i++) {
-              employeeArray.push(results[i].employee);
-            }
-            return employeeArray;
+let query =
+`Select e.id,e.first_name,e.last_name,r.title, d.name,r.salary, e.manager_id
+      
+from employees e
+join role r
+on r.id = e.role_id
+join department d
+on d.id = r.department_id
+`
+
+ connection.query(query, function(err, results) {
+  if (err) throw err; 
+  console.log("All Current employees")
+  return console.table(results)
+  
 })
-start();
-}
+
+};
+
+
 
 // function to view employees by department
 function viewDepartment(){
-  connection.query("SELECT * FROM employees", function(err, results) {
+  connection.query("SELECT * FROM department", function(err, results) {
     if (err) throw err;
+    // console.log(results)
   inquirer
-    .prompt([{
-      name: "choice",
-      type: "department",
+    .prompt({
+      name: "department",
+      type: "list",
+      message: "Which department?",
       choices: function() {
         var choiceArray = [];
         for (var i = 0; i < results.length; i++) {
-          choiceArray.push(results[i].department);
+          choiceArray.push(results[i].name);
         }
         return choiceArray;
-      }
-    }])
-
+      },
+    })
     .then(function(answer) {
-      let employeeDepartment = [];
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].department === answer.choice) {
-            employeeDepartment.push(results[i]);
-          }
-        }
-        return employeeDepartment;
+   
+      let dquery = 
+      `Select e.id,e.first_name,e.last_name,r.title, name,r.salary,e.manager_id
+      
+      from employees e
+      join role r
+      on r.id = e.role_id
+      join department d
+      on d.id = r.department_id
+
+      Where ?
+      `
+
+      connection.query(dquery,[{name: answer.department}], function(err, res) {
+        if (err) throw err;
+
+      console.table(res)
     })
  })
+
+})
 start();
-}
+};
+
 
 // function to view employees by manager
 function viewManager(){
   connection.query("SELECT * FROM employees", function(err, results) {
     if (err) throw err;
   inquirer
-    .prompt([{
-      name: "choice",
-      type: "manager",
+    .prompt({
+      name: "manager",
+      type: "list",
+      message: "Choose manager by id?",
       choices: function() {
-        let choiceArray = [];
+        var choiceMArray = [];
         for (var i = 0; i < results.length; i++) {
-          choiceArray.push(results[i].manager);
+          if(results[i].manager_id != NULL){
+          choiceMArray.push(results[i].manager_id);
+          };
         }
-        return choiceArray;
-      }
-    }])
+        return choiceMArray;
+      },
+    })
 
     .then(function(answer) {
-      let employeeManager = [];
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].department === answer.choice) {
-            employeeManager.push(results[i]);
-          }
-        }
-        return employeeManager;
+
+      let mquery = 
+      `Select e.id,e.first_name,e.last_name,r.title, d.name,r.salary,manager_id
+      from employees e
+      join role r
+      on r.id = e.role_id
+      join department d
+      on d.id = r.department_id
+
+      where ?
+      `
+
+      connection.query(mquery,[{manager_id: answer.manager}], function(err, results) {
+        if (err) throw err;
+
+      console.table(results)
     })
  })
- start();
+ })
 }
 
-// function to add a new employee
-function addEmployee() {
-  connection.query("SELECT * FROM employees", function(err, results) {
-    if (err) throw err;
-  // prompt for info about the item being put up for auction
-  inquirer
-    .prompt([
-      {
-        name: "first_name",
-        type: "input",
-        message: "What is the employee's first name?"
-      },
-      {
-        name: "last_name",
-        type: "input",
-        message: "What is the employee's last name?"
-      },
-      {
-        name: "title",
-        type: "rawlist",
-        choices: function() {
-          var titleArray = [];
-          for (var i = 0; i < results.length; i++) {
-            choiceArray.push(results[i].title);
-          }
-          return titleArray;
-        },
-        message: "What is the employee's role?"
-      },
-      {
-        name: "department",
-        type: "rawlist",
-        choices: function() {
-          var departmentArray = [];
-          for (var i = 0; i < results.length; i++) {
-            choiceArray.push(results[i].department);
-          }
-          return departmentArray;
-        },
-        message: "What department is the employee in?"
-      },     
-      {
-        name: "salary",
-        type: "input",
-        message: "What is the employee's salary?"
-      },
-      {
-        name: "manager",
-        type: "rawlist",
-        choices: function() {
-          var managerArray = [];
-          for (var i = 0; i < results.length; i++) {
-            choiceArray.push(results[i].manager);
-          }
-          return managerArray;
-        },
-        message: "Who is the employee's manager?"
-      }      
-    ])
-    .then(function(answer) {
-      // when finished prompting, insert a new item into the db with that info
-      connection.query(
-        "INSERT INTO employees SET ?",
-        {
-          first_name: answer.first_name,
-          last_name: answer.last_name,
-          title: answer.title,
-          department: answer.department,
-          salary: answer.salary,
-          manager: answer.manager,
-          
-        },
-        function(err) {
-          if (err) throw err;
-          console.log("New Empoyee Added!");
-          start();
-        }
-      );
-    });
-  })
-  start();
-}
-
-
-
-
-
-// function to renove an employee -could cause an issue if multiple employees have the same name
+// function to renove an employee 
 function deleteEmployee(){
   connection.query("SELECT * FROM employees", function(err, results) {
     if (err) throw err;
   inquirer
     .prompt([{
-      name: "choice",
-      type: "employee",
-      choices: function() {
-        var choiceArray = [];
-        for (var i = 0; i < results.length; i++) {
-          choiceArray.push(results[i].employee);
-        }
-        return choiceArray;
-      }
+      name: "employeeId",
+      type: "choice",
+      choices: results.map(employee=>
+         {return {name: employee.first_name+" "+employee.last_name, value: employee.id} })
+              
     }])
 
     .then(function(answer) {
       connection.query(
-        "Delete employee WHERE ? = ??",
+        "Delete employee WHERE ? ",
         [
           {
-            first_name: answer.bid
-          },
-          {
-            id: chosenItem.id
+            id: answer.employeeId
           }
         ],
         function(error) {
@@ -273,13 +218,8 @@ function updateRole() {
     .prompt([{
       name: "employee",
       type: "choice",
-      choices: function() {
-        let employeeArray = [];
-        for (var i = 0; i < results.length; i++) {
-          choiceArray.push(results[i].employee);
-        }
-        return choiceArray;
-      }
+      choices: results.map(employee=>
+         {return {name: employee.first_name+" "+employee.last_name, value: employee.id} })
     },
     {
       name: "role",
@@ -325,58 +265,40 @@ function updateRole() {
   });
 }
 
-// function to update the employees role
-function updateManager() {
+// function to update the employees manager
+function updateManger() {
   connection.query("SELECT * FROM employees", function(err, results) {
     if (err) throw err;
   inquirer
     .prompt([{
       name: "employee",
       type: "choice",
-      choices: function() {
-        let employeeArray = [];
-        for (var i = 0; i < results.length; i++) {
-          choiceArray.push(results[i].employee);
-        }
-        return choiceArray;
-      }
+      choices: results.map(employee=>
+         {return {name: employee.first_name+" "+employee.last_name, value: employee.id} })
     },
     {
       name: "manager",
-      type: "choice",
-      choices: function() {
-        let managerArray = [];
-        for (var i = 0; i < results.length; i++) {
-          managerArray.push(results[i].role);
-        }
-        return managerArray;
-      }
+      type: "input",
+      messager:"Input new manager ID"
     }
 
   ])
       .then(function(answer) {
-        // get the information of the chosen item
-        var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].item_name === answer.choice) {
-            chosenItem = results[i];
-          }
-        }
-          // bid was high enough, so update db, let the user know, and start over
+
+
           connection.query(
             "UPDATE employees SET ? WHERE ?",
             [
               {
-                role: answer.manager
+                manager: answer.manager
               },
               {
-                // need to fix
                 id: id
               }
             ],
             function(error) {
               if (error) throw err;
-              console.log("Employee manager updated!");
+              console.log("Employee role updated!");
               start();
             }
           );
@@ -385,6 +307,87 @@ function updateManager() {
   });
 }
 
-
+// // function to add a new employee
+// function addEmployee() {
+//   connection.query("SELECT * FROM employees", function(err, results) {
+//     if (err) throw err;
+//   // prompt for info about the item being put up for auction
+//   inquirer
+//     .prompt([
+//       {
+//         name: "first_name",
+//         type: "input",
+//         message: "What is the employee's first name?"
+//       },
+//       {
+//         name: "last_name",
+//         type: "input",
+//         message: "What is the employee's last name?"
+//       },
+//       {
+//         name: "title",
+//         type: "rawlist",
+//         choices: function() {
+//           var titleArray = [];
+//           for (var i = 0; i < results.length; i++) {
+//             choiceArray.push(results[i].title);
+//           }
+//           return titleArray;
+//         },
+//         message: "What is the employee's role?"
+//       },
+//       {
+//         name: "department",
+//         type: "rawlist",
+//         choices: function() {
+//           var departmentArray = [];
+//           for (var i = 0; i < results.length; i++) {
+//             choiceArray.push(results[i].department);
+//           }
+//           return departmentArray;
+//         },
+//         message: "What department is the employee in?"
+//       },     
+//       {
+//         name: "salary",
+//         type: "input",
+//         message: "What is the employee's salary?"
+//       },
+//       {
+//         name: "manager",
+//         type: "rawlist",
+//         choices: function() {
+//           var managerArray = [];
+//           for (var i = 0; i < results.length; i++) {
+//             choiceArray.push(results[i].manager);
+//           }
+//           return managerArray;
+//         },
+//         message: "Who is the employee's manager?"
+//       }      
+//     ])
+//     .then(function(answer) {
+//       // when finished prompting, insert a new item into the db with that info
+//       connection.query(
+//         "INSERT INTO employees SET ?",
+//         {
+//           first_name: answer.first_name,
+//           last_name: answer.last_name,
+//           title: answer.title,
+//           department: answer.department,
+//           salary: answer.salary,
+//           manager: answer.manager,
+          
+//         },
+//         function(err) {
+//           if (err) throw err;
+//           console.log("New Empoyee Added!");
+//           start();
+//         }
+//       );
+//     });
+//   })
+//   start();
+// }
 
 
--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -7,9 +7,21 @@ CREATE TABLE employees(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100) NOT NULL,
-  title_name VARCHAR(100) NOT NULL,
-  department VARCHAR(100) NOT NULL,
-  salary INT default 0,
-  first_name VARCHAR(100) NOT NULL,
+  role_id INT default 0,
+  manager_id INT default 0,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) default 0,
+  department_id INT default 0,
   PRIMARY KEY (id)
 );
